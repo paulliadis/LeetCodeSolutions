25d3ed183258665810b903eb1d9baf4e42c27ae0
bigdirkmalone <bigdirkmalone@gmail.com>
2022-10-21 23:39:54 -0400

    added containsduplicatetwo.cs

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -13,7 +13,7 @@ namespace LeetCodeSolutions
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new topkwords());
+            Application.Run(new containsduplicatetwo());
         }
     }
 }
---
+++ LeetCodeSolutions/containsduplicatetwo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeetCodeSolutions
+{
+    public partial class containsduplicatetwo : Form
+    {
+        public containsduplicatetwo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] nums = { 1, 2, 3, 1, 2, 3 };
+            int k = 2;
+            bool answer = ContainsNearbyDuplicate(nums, k);
+
+            System.Diagnostics.Debug.WriteLine(answer);
+        }
+
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+
+            int numsPointer = 0; // Stores position in the array of number being observed
+
+            while(numsPointer < nums.Length)
+            {
+                int foundPosition = Array.IndexOf(nums, nums[numsPointer], numsPointer + 1);
+
+                if((Math.Abs(foundPosition - numsPointer) <= k)&& foundPosition > 0)
+                {
+                    return true;
+                }
+
+                numsPointer++;
+            }
+
+            return false;
+        }
+            
+
+    }
+}
