cce59fa74e6a41f0ddfa620bb8d707847c546eb1
bigdirkmalone <bigdirkmalone@gmail.com>
2022-10-25 09:34:27 -0400

    Added arraystringsareequal.cs.

---
+++ LeetCodeSolutions/arraystringsareequal.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace LeetCodeSolutions
+{
+    partial class arraystringsareequal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(347, 218);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 23);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Answer:";
+            // 
+            // arraystringsareequal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.button1);
+            this.Name = "arraystringsareequal";
+            this.Text = "arraystringsareequal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox txtAnswer;
+        private Label label1;
+    }
+}
---
+++ LeetCodeSolutions/arraystringsareequal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeetCodeSolutions
+{
+    public partial class arraystringsareequal : Form
+    {
+        public arraystringsareequal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //************************************************************************************************************************
+            //* https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+            //* Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+            //* A string is represented by an array if the array elements concatenated in order forms the string.
+            //************************************************************************************************************************
+
+            string[] word1 = { "abc", "d", "defg" };
+            string[] word2 = { "abcddefg" };
+  
+            txtAnswer.Text = ArrayStringsAreEqual(word1 , word2).ToString();
+        }
+
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            string concatWord1 = string.Concat(word1);
+            string concatWord2 = string.Concat(word2);
+
+            if(concatWord1 == concatWord2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+}
